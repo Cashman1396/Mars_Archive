dbbfdff09adbaa4726bec293c81da1152ac9bd76
Cashman1396 <Chrisashman96@gmail.com>
2021-03-10 14:36:10 -0500

    added information for index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
+//REACT imports
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+//REDUX imports
+
+
+import thunk from 'react-redux'
+
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk))
+);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
